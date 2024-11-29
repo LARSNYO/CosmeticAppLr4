8a49c4d1e2740a03bc9f061cb381dc5c2ead129b
LARSNYO <LARSNYO@AKRASIA>
2024-11-29 22:32:52 +0300

    Добавьте файлы проекта.

---
+++ CosmeticAppLr4/Controllers/CreamsController.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using CosmeticAppLr4.Models;
+using Microsoft.VisualBasic;
+
+namespace CosmeticAppLr4.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CreamsController : ControllerBase
+    {
+        private readonly CreamContext _context;
+        public CreamsController(CreamContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Cream>>> GetCreams(string name)
+        {
+            return await _context.Creams.Where(cream => name == null || cream.Name == name).ToListAsync();
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Cream>> GetCream(int id)
+        {
+            var cream = await _context.Creams.FindAsync(id);
+            if (cream == null)
+            {
+                return NotFound();
+            }
+            return cream;
+        }
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutCream(int id, Cream cream)
+        {
+            if (id != cream.Id)
+            {
+                return BadRequest();
+            }
+            _context.Entry(cream).State = EntityState.Modified;
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CreamExists(id))
+                {
+                    return NotFound();
+                }
+                throw;
+            }
+            return CreatedAtAction("GetCream", new { id = cream.Id }, cream);
+        }
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCream(int id) 
+        {
+            var cream = await _context.Creams.FindAsync(id);
+            if(cream == null)
+            {
+                return NotFound();
+            }
+            _context.Creams.Remove(cream);
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+        private bool CreamExists(int id)
+        {
+            return _context.Creams.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ CosmeticAppLr4/Models/CreamContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CosmeticAppLr4.Models
+{
+    public class CreamContext : DbContext
+    {
+        public CreamContext(DbContextOptions<CreamContext> options) : base(options) { }
+        public DbSet<Cream> Creams { get; set; }
+    }
+}
